2868b033f43f8444332acd2198ae43e6870f60f2
Vinay Simha BN <simhavcs@gmail.com>
2015-07-25 11:52:41 +0530

    WIP: Testing hdmi not called in modeset in mdp4_kms.c

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -973,8 +973,8 @@ static int msm_pdev_probe(struct platform_device *pdev)
 	 * care about apq8064/apq8060/etc (all mdp4/a3xx):
 	 */
 	static const char *devnames[] = {
-		/*"hdmi_msm.0", "kgsl-3d0.0",*/
-		"mipi_dsi.0", "kgsl-3d0.0",
+		"hdmi_msm.0", "kgsl-3d0.0",
+		/*"mipi_dsi.0", "kgsl-3d0.0", TODO : FIX*/
 	};
 	int i;
 
@@ -1033,9 +1033,9 @@ static int __init msm_drm_register(void)
 {
 	DBG("init");
 	dsi_register();
-#ifdef CONFIG_DRM_MSM_HDMI
+//#ifdef CONFIG_DRM_MSM_HDMI
 	hdmi_register();
-#endif
+//#endif
 	a3xx_register();
 	return platform_driver_register(&msm_platform_driver);
 }
@@ -1045,9 +1045,9 @@ static void __exit msm_drm_unregister(void)
 	DBG("fini");
 	platform_driver_unregister(&msm_platform_driver);
 	dsi_unregister();
-#ifdef CONFIG_DRM_MSM_HDMI
+//#ifdef CONFIG_DRM_MSM_HDMI
 	hdmi_unregister();
-#endif
+//#endif
 	a3xx_unregister();
 }
 
